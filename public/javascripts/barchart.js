1364c3bbac360ee5e1093794378453fc6961ae91
Ken Chan <KChan923@gmail.com>
2015-04-29 13:25:27 -0400

    fixed minor dropdown click issue

--- public/javascripts/barchart.js
+++ public/javascripts/barchart.js
@@ -377,4 +377,15 @@
         generateBarChart("hours_model_flown_90_days", "Total Hours Flown (Past 90 Days) - Make/Model");
     }); 
 
+  $(document).mouseup(function (e)
+  {
+    var container = $("#dropdown3");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.hide();
+    }
+  });
+
 })();
